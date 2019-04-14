fe2ee19c5441ec9933bddfd404befd93d74783eb
siva600 <vsivachaitanya60@gmail.com>
2019-04-14 10:24:08 -0700

    updating

--- quicksort.py
+++ quicksort.py
@@ -11,7 +11,8 @@ def quicksort(array):
     if len(array) < 2:
         return array
     else:
-        pivot = array[0]
+        # pivot = array[0]
+	pivot = array[0]
         less = [i for i in array[1:] if i <= pivot]
         greater = [i for i in array[1:] if i >= pivot]
         # print("less array", less)
